285d03c7b6e76de7bb52fcd7323e9fa4e74bd90f
Yixiu Jiang <yixiu.jiang@gmail.com>
2017-10-17 21:41:22 +1100

    useing h2 db for test

--- src/test/java/spaceshuttle/UserControllerIT.java
+++ src/test/java/spaceshuttle/UserControllerIT.java
@@ -12,11 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PropertySource("h2-db.properties")
 public class UserControllerIT {
 
     @LocalServerPort
