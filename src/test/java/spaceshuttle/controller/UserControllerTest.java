1b4a13b159ae7f5f4f3967486232dc74e93b1318
Charlie Jiang <yixiujiang@gmail.com>
2017-10-18 15:17:16 +1100

    update test cases

--- src/test/java/spaceshuttle/controller/UserControllerTest.java
+++ src/test/java/spaceshuttle/controller/UserControllerTest.java
@@ -1,23 +1,22 @@
-package spaceshuttle;
-
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+package spaceshuttle.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = UserController.class)
 @AutoConfigureMockMvc
 @TestPropertySource("classpath:h2-db.properties")
 public class UserControllerTest {
