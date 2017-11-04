48d9d36c1763f5befade54e0b281d8914b4baf72
Yixiu Jiang <yixiu.jiang@gmail.com>
2017-11-04 23:23:06 +1100

    add vaadin

--- src/main/java/spaceshuttle/model/Customer.java
+++ src/main/java/spaceshuttle/model/Customer.java
@@ -11,7 +11,8 @@ public class Customer {
     public String firstName;
     public String lastName;
 
-    public Customer() {}
+    public Customer() {
+    }
 
     public Customer(String firstName, String lastName) {
         this.firstName = firstName;
@@ -25,5 +26,28 @@ public class Customer {
                 id, firstName, lastName);
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
 
--- src/main/java/spaceshuttle/repository/CustomerRepository.java
+++ src/main/java/spaceshuttle/repository/CustomerRepository.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
-     Customer findByFirstName(String firstName);
-     List<Customer> findByLastName(String lastName);
+    Customer findByFirstName(String firstName);
 
+    List<Customer> findByLastName(String lastName);
+
+    List<Customer> findByLastNameStartsWithIgnoreCase(String lastName);
 }
