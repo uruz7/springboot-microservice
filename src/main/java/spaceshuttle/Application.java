0ffd0359578845b0d83976e7808697fdbea7b18c
Charlie Jiang <yixiujiang@gmail.com>
2017-11-13 11:11:37 +1100

    fix merge issue

--- src/main/java/spaceshuttle/Application.java
+++ src/main/java/spaceshuttle/Application.java
@@ -2,6 +2,7 @@ package spaceshuttle;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
