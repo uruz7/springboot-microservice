79886330af96bf4b42e8ec63d39a1ec5b5e16f1c
Yixiu Jiang <yixiu.jiang@gmail.com>
2017-11-14 22:44:18 +1100

    remove mongodb initializer

--- src/main/java/spaceshuttle/Application.java
+++ src/main/java/spaceshuttle/Application.java
@@ -1,14 +1,11 @@
 package spaceshuttle;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import spaceshuttle.model.Customer;
-import spaceshuttle.repository.CustomerRepository;
 
 import java.util.Arrays;
 
@@ -20,8 +17,8 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Autowired
-    private CustomerRepository repository;
+//    @Autowired
+//    private CustomerRepository repository;
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
@@ -34,30 +31,30 @@ public class Application {
             for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
-            repository.deleteAll();
-
-            // save a couple of customers
-            repository.save(new Customer("Alice", "Smith"));
-            repository.save(new Customer("Bob", "Smith"));
-
-            // fetch all customers
-            System.out.println("Customers found with findAll():");
-            System.out.println("-------------------------------");
-            for (Customer customer : repository.findAll()) {
-                System.out.println(customer);
-            }
-            System.out.println();
-
-            // fetch an individual customer
-            System.out.println("Customer found with findByFirstName('Alice'):");
-            System.out.println("--------------------------------");
-            System.out.println(repository.findByFirstName("Alice"));
-
-            System.out.println("Customers found with findByLastName('Smith'):");
-            System.out.println("--------------------------------");
-            for (Customer customer : repository.findByLastName("Smith")) {
-                System.out.println(customer);
-            }
+//            repository.deleteAll();
+//
+//            // save a couple of customers
+//            repository.save(new Customer("Alice", "Smith"));
+//            repository.save(new Customer("Bob", "Smith"));
+//
+//            // fetch all customers
+//            System.out.println("Customers found with findAll():");
+//            System.out.println("-------------------------------");
+//            for (Customer customer : repository.findAll()) {
+//                System.out.println(customer);
+//            }
+//            System.out.println();
+//
+//            // fetch an individual customer
+//            System.out.println("Customer found with findByFirstName('Alice'):");
+//            System.out.println("--------------------------------");
+//            System.out.println(repository.findByFirstName("Alice"));
+//
+//            System.out.println("Customers found with findByLastName('Smith'):");
+//            System.out.println("--------------------------------");
+//            for (Customer customer : repository.findByLastName("Smith")) {
+//                System.out.println(customer);
+//            }
 
 
         };
